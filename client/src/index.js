e890ff2867ebf9114bb5ca8494ad2c0cc04d6387
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-04-19 03:30:37 +0200

    client setup

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let jsx = (
+  <body>
+    <h1>Hello world ;)</h1>
+  </body>
+);
+
+ReactDOM.render(jsx, document.getElementById("root"));
